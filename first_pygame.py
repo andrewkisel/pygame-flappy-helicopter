d2dd58324c1960ac1dc39c84f627d68fa256d4cb
Andrew Kisel <andrew@kisels.net>
2019-02-07 14:09:46 -0500

    Add helicopter image, place it on the surface

--- first_pygame.py
+++ first_pygame.py
@@ -1,5 +1,8 @@
 import pygame
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+
 # Initializing.
 pygame.init()
 # Define game surface.
@@ -9,15 +12,26 @@ pygame.display.set_caption('Helicopter')
 # Tracking time for FPS.
 clock = pygame.time.Clock()
 
-# Game loop
+
+def helicopter(x, y, image):
+    surface.blit(img, (x, y))
+
+
+img = pygame.image.load('helicopter.png')
+# Helicopter starting points
+x, y = 150, 200
+
 game_over = False
 
+# Game loop
 while not game_over:
     # Looping through events tracked by pygame.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    surface.fill(black)
+    helicopter(x, y, img)
     # Updating specific areas on the screen. If empty parameters - everything is updated.
     pygame.display.update()
     # Setting FPS
