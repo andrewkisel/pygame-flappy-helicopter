ab2747c8700b5688491c844c976638f4d80a7fad
Andrew Kisel <andrew@kisels.net>
2019-02-07 14:39:36 -0500

    Add logic to handle boundries, logic to handle helicopter movements

--- first_pygame.py
+++ first_pygame.py
@@ -11,15 +11,22 @@ surface = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Helicopter')
 # Tracking time for FPS.
 clock = pygame.time.Clock()
+# Loading helicopter image
+img = pygame.image.load('helicopter.png')
+# Helicopter starting points
+x, y = 150, 200
+
+# Variable to render helicopter movements along y axis.
+y_move = 5
+
+
+def gameover():
+    quit()
 
 
 def helicopter(x, y, image):
     surface.blit(img, (x, y))
 
-
-img = pygame.image.load('helicopter.png')
-# Helicopter starting points
-x, y = 150, 200
 
 game_over = False
 
@@ -29,9 +36,27 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        # Case when pressing a key.
+        if event.type == pygame.KEYDOWN:
+            # Get specific key that is pressed.
+            if event.key == pygame.K_UP:
+                y_move = -5
+        # Case when releasing the key.
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                y_move = 5
 
+    y += y_move
+
+    # Fill the background.
     surface.fill(black)
+    # Put a helicopter icon on the surface.
     helicopter(x, y, img)
+
+    # Check if helicopter went out of boundaries.
+    if y > 360 or y < 0:
+        gameover()
+
     # Updating specific areas on the screen. If empty parameters - everything is updated.
     pygame.display.update()
     # Setting FPS
