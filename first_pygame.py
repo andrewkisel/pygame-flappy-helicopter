5a6586774594033c95a2ad25831f7fafca182c49
Andrew Kisel <andrew@kisels.net>
2019-02-08 10:55:35 -0500

    Add logic to handle crashes into blocks. Modify picture size to be more precise. Make gap size bigger.

--- first_pygame.py
+++ first_pygame.py
@@ -4,14 +4,15 @@ import random
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255, 0, 0)
 
 # Initializing.
 pygame.init()
 
 surface_width = 800
 surface_height = 500
-image_height = 64
-image_width = 64
+image_height = 32
+image_width = 32
 
 # Define game surface.
 surface = pygame.display.set_mode((surface_width, surface_height))
@@ -28,7 +29,7 @@ img = pygame.image.load('helicopter.png')
 
 def blocks(x_block, y_block, block_width, block_height, gap):
     pygame.draw.rect(surface, white, [x_block, y_block, block_width, block_height])
-    pygame.draw.rect(surface, white, [x_block, y_block + block_height + gap, block_width, block_height])
+    pygame.draw.rect(surface, white, [x_block, y_block + block_height + gap, block_width, surface_height])
 
 
 # Handle press any key to continue.
@@ -45,7 +46,7 @@ def replay_or_quit():
 
 # Create text objects and return them in addition to rectangles.
 def make_text_objs(message, font):
-    text_surface = font.render(message, True, white)
+    text_surface = font.render(message, True, red)
     return text_surface, text_surface.get_rect()
 
 
@@ -89,13 +90,15 @@ def main():
     # Variable to render helicopter movements along y axis.
     y_move = 0
 
+    # Block coordinates and size
     x_block = surface_width
     y_block = 0
     block_width = 75
-    block_height = random.randint(0, surface_height)
-    gap = image_height * 2
+    block_height = random.randint(0, (surface_height / 2))
+    gap = image_height * 4
+
     # How fast the blocks move.
-    block_move = 3
+    block_move = 5
 
     game_over = False
 
@@ -129,6 +132,26 @@ def main():
         # Check if helicopter went out of boundaries.
         if y > surface_height - 64 or y < 0:
             gameover()
+        # Check if block has moved out of the screen.
+        if x_block < (-1 * block_width):
+            # Resetting the starting x-position of the block.
+            x_block = surface_width
+            block_height = random.randint(0, (surface_height / 2))
+
+        # Check if we crashed into the block.
+        if x + image_width > x_block:
+            if x < x_block + block_width:
+                print('Possibly within the boundaries of x')
+                if y < block_height:
+                    print('y crossover upper!')
+                    if x - image_width < block_width + x_block:
+                        print('game over. hit upper.')
+                        gameover()
+            if y + image_height > block_height + gap:
+                print('y crossover lower')
+                if x < block_width + x_block:
+                    print('Game over lower')
+                    gameover()
 
         # Updating specific areas on the screen. If empty parameters - everything is updated.
         pygame.display.update()
